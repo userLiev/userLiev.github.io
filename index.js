2f07db59373c446a55ebea3ce9b911a5fffa0423
Gabriel Coelho <gabriellc2307@gmail.com>
2021-01-23 20:25:26 -0300

    less

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ function onBirthday(e) {
   const now = new Date().getTime();
   const birth = new Date(el.value).getTime();
   const diff = parseInt((now - birth)/(24 * 3600 * 1000 * 7));
+  const less = parseInt(diff / (52 * 5));
 
   const cells = document.querySelectorAll('.cell');
 
   for (let i = 0; i < cells.length; i++) {
-    if (i < diff) {
+    if (i < (diff - less)) {
       cells[i].classList.add('paint');
     } else {
       cells[i].classList.remove('paint');
